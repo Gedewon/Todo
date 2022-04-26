f7e3d26a864945edd681a8a5bf6a1b106ef39a2e
Gedewon <gedewon.hayle.9@gmail.com>
2022-04-26 22:51:19 +0300

    a little tweak to fix linter issue

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import "./style.css";
+import './style.css';
 
 const todoList = [
-  { description: "buy laptop", completed: false, index: 0 },
-  { description: "read books", completed: false, index: 1 },
-  { description: "study c#", completed: false, index: 2 },
+  { description: 'buy laptop', completed: false, index: 0 },
+  { description: 'read books', completed: false, index: 1 },
+  { description: 'study c#', completed: false, index: 2 },
 ];
 
 function render() {
-  let screenElement = "";
-  for (const list of todoList) {
+  let screenElement = '';
+  todoList.forEach((list) => {
     screenElement += `<li id=${list.index}><p><input type="checkbox" name="" id="">${list.description}</p>&vellip;</li>`;
-  }
+  });
   return screenElement;
 }
-// if the list is not ordered i will use sorting
-window.onload = function () {
-  const root = document.querySelector(".todos-list");
+
+window.onload = () => {
+  const root = document.querySelector('.todos-list');
   root.innerHTML = render();
 };
