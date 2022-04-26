d36758474f4e46051b9a85fbc1267d939afe56fd
Gedewon <gedewon.hayle.9@gmail.com>
2022-04-26 20:44:42 +0300

    crate simple tasks

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import "./style.css";
 
+const todoList = [
+  { description: "buy laptop", completed: false, index: 0 },
+  { description: "read books", completed: false, index: 1 },
+  { description: "study c#", completed: false, index: 2 },
+];
+
 function component() {
   const element = document.createElement("div");
 
