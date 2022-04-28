85a6088f298dd867d196f2d1b603e818e6f79c9d
Gedewon <gedewon.hayle.9@gmail.com>
2022-04-28 14:06:50 +0300

    Change delete icon

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
-import './style.css';
-import TodoList from './utils.js';
+import "./style.css";
+import TodoList from "./utils.js";
 
-const root = document.querySelector('.todos-list');
-const taskInput = document.querySelector('#input-list');
+const root = document.querySelector(".todos-list");
+const taskInput = document.querySelector("#input-list");
 
 const todoList = new TodoList();
 
 function render() {
-  let screenElement = '';
+  let screenElement = "";
   todoList.getTasks().forEach((list) => {
-    screenElement += `<li  id=${list.index}><input type="checkbox" name="" ><input class="each-list" id=${list.index} type="text" contenteditable="true" value=${list.description}></input><span class="move-delete" id=${list.index}>&vellip;</span></li>`;
+    screenElement += `<li  id=${list.index}><input type="checkbox" name="" ><input class="each-list" id=${list.index} type="text" contenteditable="true" value=${list.description}></input><span class="move-delete" id=${list.index}>&#x02297;
+    </span></li>`;
   });
   return screenElement;
 }
 
 function init() {
   root.innerHTML = render();
-  document.querySelectorAll('.move-delete').forEach((el) => {
-    el.addEventListener('click', (e) => {
+  document.querySelectorAll(".move-delete").forEach((el) => {
+    el.addEventListener("click", (e) => {
       todoList.remove(e.target.id);
     });
   });
-  document.querySelectorAll('.each-list').forEach((el) => {
-    el.addEventListener('change', (e) => {
+  document.querySelectorAll(".each-list").forEach((el) => {
+    el.addEventListener("change", (e) => {
       todoList.edit(e.target.value, e.target.id);
     });
   });
@@ -30,7 +31,9 @@ function init() {
 
 window.onload = init();
 
-taskInput.addEventListener('change', (e) => {
-  todoList.addTask(e.target.value, todoList.getIndex());
-  init();
+taskInput.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    todoList.addTask(e.target.value, todoList.getIndex());
+    init();
+  }
 });
