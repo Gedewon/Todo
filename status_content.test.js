63dbd375218c88dd70d4b4d39b791420dbc57468
Gedewon <gedewon.hayle.9@gmail.com>
2022-05-06 00:52:02 +0300

    Test edit and status change

--- status_content.test.js
+++ status_content.test.js
@@ -9,8 +9,22 @@ const todoList = new TodoList([
 ]);
 
 describe("Status and task Change", () => {
-  test("change status of task", () => {
+  test("change item completed status ", () => {
     todoList.markAsDone(todoList.todoList[0].index);
     expect(todoList.getTasks()[0].completed).toBe(true);
+  });
+
+  test("should edit task description ", () => {
+    todoList.edit("read book", 1);
+    expect(localStorage.setItem).toHaveBeenLastCalledWith(
+      "List",
+      JSON.stringify([
+        {
+          description: "read book",
+          index: 1,
+          completed: true,
+        },
+      ])
+    );
   });
 });
