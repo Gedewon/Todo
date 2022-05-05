ecc216912c1ea9bb46601d4cf4ced75fc96f8f90
Gedewon <gedewon.hayle.9@gmail.com>
2022-05-05 23:07:52 +0300

    Add test method for remove

--- add_delet.test.js
+++ add_delet.test.js
@@ -1,8 +1,18 @@
 import TodoList from "./modules/TodoList.js";
 const todoList = new TodoList();
 
-describe("add method should store in class field", () => {
-  test("test add ", () => {
+// beforeEach(() => {
+//   // to fully reset the state between tests, clear the storage
+//   localStorage.clear();
+//   // and reset all mocks
+//   jest.clearAllMocks();
+
+//   // clearAllMocks will impact your other mocks too, so you can optionally reset individual mocks instead:
+//   localStorage.setItem.mockClear();
+// });
+
+describe("Add task method", () => {
+  test("add method should store in class field", () => {
     let task = [
       {
         description: "Read you don't know js",
@@ -22,5 +32,17 @@ describe("add method should store in class field", () => {
   });
   test("add method should store to localstorage ", () => {
     expect(localStorage.length).not.toBe(0);
+  });
+});
+
+describe("Delete task method", () => {
+  test("Delete method should remove from  class field", () => {
+    todoList.remove(1);
+
+    expect(todoList.getTasks().length).toBe(0);
+  });
+  test("Delete method should remove from localStorage ", () => {
+    // console.log(localStorage);
+    expect(localStorage).toEqual({ List: "[]" });
   });
 });
