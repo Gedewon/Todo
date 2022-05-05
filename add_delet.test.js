ad054be5751dcc881b93751a1848c40d01daa28b
Gedewon <gedewon.hayle.9@gmail.com>
2022-05-05 22:49:31 +0300

    Add test for addmethod

--- add_delet.test.js
+++ add_delet.test.js
@@ -1,17 +1,26 @@
-const TodoList = require("./modules/TodoList");
+import TodoList from "./modules/TodoList.js";
 const todoList = new TodoList();
-test("test add ", () => {
-  //    arrange
-  let task = {
-    description: "Read you don't know js",
-    index: 1,
-  };
 
-  todoList.addTask(...task);
+describe("add method should store in class field", () => {
+  test("test add ", () => {
+    let task = [
+      {
+        description: "Read you don't know js",
+        index: 1,
+      },
+    ];
 
-  expect(todoList.getTasks()).toEqual({
-    description: "Read you don't know js",
-    index: 1,
-    completed: false,
+    todoList.addTask(task[0].description, task[0].index);
+
+    expect(todoList.getTasks()).toEqual([
+      {
+        description: "Read you don't know js",
+        index: 1,
+        completed: false,
+      },
+    ]);
+  });
+  test("add method should store to localstorage ", () => {
+    expect(localStorage.length).not.toBe(0);
   });
 });
